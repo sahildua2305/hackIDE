3620fa99281ed002a26d08c1c38a8e507fe7c455
Sahil Dua <sahildua2305@gmail.com>
2016-01-06 01:43:58 +0530

    Added ace-builds files

--- hackIDE/views.py
+++ hackIDE/views.py
@@ -3,15 +3,12 @@
 # @Author: sahildua2305
 # @Date:   2016-01-06 00:11:27
 # @Last Modified by:   sahildua2305
-# @Last Modified time: 2016-01-06 00:44:32
+# @Last Modified time: 2016-01-06 00:54:58
 
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
 
 def index(request):
-	context = {}
 	# render the index.html
-	# template = loader.get_template('hackIDE/index.html')
-	# return HttpResponse(template.render(context, request))
 	return render(request, 'hackIDE/index.html', {})
