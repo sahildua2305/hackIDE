7d2b626960eceed9ae64e8fb41bdb2ca5bcd29e2
Sahil Dua <sahildua2305@gmail.com>
2016-01-14 18:29:31 +0530

    Added is_ajax() check for request in views

--- hackIDE/views.py
+++ hackIDE/views.py
@@ -3,7 +3,7 @@
 # @Author: sahildua2305
 # @Date:   2016-01-06 00:11:27
 # @Last Modified by:   sahildua2305
-# @Last Modified time: 2016-01-14 02:59:49
+# @Last Modified time: 2016-01-14 18:28:36
 
 
 from django.shortcuts import render
@@ -66,29 +66,32 @@ Method catering to AJAX call at /ide/compile/ endpoint,
 makes call at HackerEarth's /compile/ endpoint and returns the compile result as a JsonResponse object
 """
 def compileCode(request):
-	try:
-		source = request.POST['source']
-		# Handle Empty Source Case
-		source_empty_check(source)
-		
-		lang = request.POST['lang']
-		# Handle Invalid Language Case
-		lang_valid_check(lang)
+	if request.is_ajax():
+		try:
+			source = request.POST['source']
+			# Handle Empty Source Case
+			source_empty_check(source)
+			
+			lang = request.POST['lang']
+			# Handle Invalid Language Case
+			lang_valid_check(lang)
 
-	except KeyError:
-		# Handle case when at least one of the keys (lang or source) is absent
-		missing_argument_error()
+		except KeyError:
+			# Handle case when at least one of the keys (lang or source) is absent
+			missing_argument_error()
 
+		else:
+			compile_data = {
+				'client_secret': CLIENT_SECRET,
+				'async': 0,
+				'source': source,
+				'lang': lang,
+			}
+
+			r = requests.post(COMPILE_URL, data=compile_data)
+			return JsonResponse(r.json(), safe=False)
 	else:
-		compile_data = {
-			'client_secret': CLIENT_SECRET,
-			'async': 0,
-			'source': source,
-			'lang': lang,
-		}
-
-		r = requests.post(COMPILE_URL, data=compile_data)
-		return JsonResponse(r.json(), safe=False)
+		return HttpResponseForbidden();
 
 
 """
@@ -96,40 +99,43 @@ Method catering to AJAX call at /ide/run/ endpoint,
 makes call at HackerEarth's /run/ endpoint and returns the run result as a JsonResponse object
 """
 def runCode(request):
-	try:
-		source = request.POST['source']
-		# Handle Empty Source Case
-		source_empty_check(source)
-		
-		lang = request.POST['lang']
-		# Handle Invalid Language Case
-		lang_valid_check(lang)
+	if request.is_ajax():
+		try:
+			source = request.POST['source']
+			# Handle Empty Source Case
+			source_empty_check(source)
+			
+			lang = request.POST['lang']
+			# Handle Invalid Language Case
+			lang_valid_check(lang)
 
-	except KeyError:
-		# Handle case when at least one of the keys (lang or source) is absent
-		missing_argument_error()
+		except KeyError:
+			# Handle case when at least one of the keys (lang or source) is absent
+			missing_argument_error()
 
+		else:
+			# default value of 5 sec, if not set
+			time_limit = request.POST.get('time_limit', 5)
+			# default value of 262144KB (256MB), if not set
+			memory_limit = request.POST.get('memory_limit', 262144)
+
+			run_data = {
+				'client_secret': CLIENT_SECRET,
+				'async': 0,
+				'source': source,
+				'lang': lang,
+				'time_limit': time_limit,
+				'memory_limit': memory_limit,
+			}
+
+			# if input is present in the request
+			if 'input' in request.POST:
+				run_data['input'] = request.POST['input']
+
+			"""
+			Make call to /run/ endpoint of HackerEarth API
+			"""
+			r = requests.post(RUN_URL, data=run_data)
+			return JsonResponse(r.json(), safe=False)
 	else:
-		# default value of 5 sec, if not set
-		time_limit = request.POST.get('time_limit', 5)
-		# default value of 262144KB (256MB), if not set
-		memory_limit = request.POST.get('memory_limit', 262144)
-
-		run_data = {
-			'client_secret': CLIENT_SECRET,
-			'async': 0,
-			'source': source,
-			'lang': lang,
-			'time_limit': time_limit,
-			'memory_limit': memory_limit,
-		}
-
-		# if input is present in the request
-		if 'input' in request.POST:
-			run_data['input'] = request.POST['input']
-
-		"""
-		Make call to /run/ endpoint of HackerEarth API
-		"""
-		r = requests.post(RUN_URL, data=run_data)
-		return JsonResponse(r.json(), safe=False)
+		return HttpResponseForbidden()
--- hackIDE_project/urls.py
+++ hackIDE_project/urls.py
@@ -18,7 +18,6 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    # url(r'^$', include('hackIDE.urls')),
-	url(r'^ide/', include('hackIDE.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('hackIDE.urls')),
 ]
