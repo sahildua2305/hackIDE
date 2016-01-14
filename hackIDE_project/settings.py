0d02cf7eb957a9078a3567430602db04b21440a8
Sahil Dua <sahildua2305@gmail.com>
2016-01-14 18:03:41 +0530

    Changed settings.py

--- hackIDE_project/settings.py
+++ hackIDE_project/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # Enabling whitenoise with gzip and caching support
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
