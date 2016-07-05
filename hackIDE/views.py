83af2267a05d1ad02275be04cf8842a81fa4d8cc
JSSaini07 <mr.jssaini07@gmail.com>
2016-07-05 22:47:00 +0530

    implemented savedCodeView and modified runCode to save the code

--- hackIDE/views.py
+++ hackIDE/views.py
@@ -20,8 +20,7 @@ RUN_URL = "https://api.hackerearth.com/v3/code/run/"
 # access config variable
 DEBUG = (os.environ.get('HACKIDE_DEBUG') != None)
 # DEBUG = (os.environ.get('HACKIDE_DEBUG') or "").lower() == "true"
-#CLIENT_SECRET = os.environ['HE_CLIENT_SECRET'] if not DEBUG else ""
-CLIENT_SECRET = "bea98ae9dcbc79df09271f0ffc5bfe7e13c2a2c6"
+CLIENT_SECRET = os.environ['HE_CLIENT_SECRET'] if not DEBUG else ""
 
 permitted_languages = ["C", "CPP", "CSHARP", "CLOJURE", "CSS", "HASKELL", "JAVA", "JAVASCRIPT", "OBJECTIVEC", "PERL", "PHP", "PYTHON", "R", "RUBY", "RUST", "SCALA"]
 
