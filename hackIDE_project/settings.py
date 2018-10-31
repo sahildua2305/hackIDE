c5fc3b65cd8997d3b03fc0ed8c1700831b1424cc
SHIVAM <41147563+shivamsahu4u@users.noreply.github.com>
2018-10-31 22:41:00 +0530

    my changes

--- hackIDE_project/settings.py
+++ hackIDE_project/settings.py
@@ -138,3 +138,5 @@ STATICFILES_DIRS = (
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+#My changes.
